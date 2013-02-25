be1ca0bfe8d05266ac6d29dacfbe1fba85581a09
itsdrewmiller <drew@drewmiller.net>
2013-02-24 21:43:01 -0500

    js cleanup

--- queue.js
+++ queue.js
@@ -6,7 +6,7 @@
 
         this.enqueue = function (val) {
             innerArray.push(val);
-        }
+        };
 
         this.cheat = function (val) {
             if (index > 0) {
@@ -16,36 +16,36 @@
                 innerArray.splice(0, 0, val);
             }
 
-        }
+        };
 
         this.dequeue = function () {
-            var returnValue = undefined;
+            var returnValue;
             while (returnValue === undefined && index < innerArray.length) {
                 index++;
                 returnValue = innerArray[index - 1];
                 innerArray[index - 1] = undefined;
             }
             return returnValue;
-        }
+        };
 
         this.peek = function (i) {
             return innerArray[index + i];
-        }
+        };
 
         this.length = function () {
             return innerArray.length - index;
-        }
+        };
 
         this.removeAt = function (i) {
             innerArray[index + i] = undefined;
-        }
+        };
 
         this.all = function (process) {
             for (var i = index; i < innerArray.length; i++) {
                 process(innerArray[i], i - index);
             }
             this.clean();
-        }
+        };
 
         this.clean = function () {
             var newArray = [];
@@ -54,11 +54,11 @@
             }
             innerArray = newArray;
             index = 0;
-        }
+        };
 
         this.sort = function (sortFunc) {
             this.clean();
             innerArray.sort(sortFunc);
-        }
-    }
+        };
+    };
 })();
